c9b6fa233219cb64c4de35ebcd6738f10dbfb7f6
root <g.drivas@gmail.com>
2021-05-19 18:05:30 +0000

    process data functions added

--- data/process_data.py
+++ data/process_data.py
@@ -1,15 +1,34 @@
 import sys
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = messages.merge(categories, on = 'id')
+    return df
 
 
 def clean_data(df):
-    pass
+    categories_df = df.categories.str.split(';',expand=True)
+    row = categories_df.iloc[0]
+    category_colnames = [cat[:-2] for cat in row]
+    categories_df.columns = category_colnames
+    for column in categories_df:
+        # set each value to be the last character of the string
+        categories_df[column] = categories_df[column].str[-1] 
+        # convert column from string to numeric
+        categories_df[column].astype(int)
+    df.drop(['categories'],axis=1,inplace=True)
+    df = pd.concat([df,categories_df],axis=1)
+    df.drop_duplicates(inplace=True)
+    #df = df[df.related.isnull() == False]
+    return df
 
 
 def save_data(df, database_filename):
+    engine = create_engine('sqlite:///'+database_filename)
+    df.to_sql('Messages', engine, index=False)
     pass  
 
 
